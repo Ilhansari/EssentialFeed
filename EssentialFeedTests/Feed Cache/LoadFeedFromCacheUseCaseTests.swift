41db2d7a150939bb61d2b0aa2172bfde679ddfeb
Ilhansari <ilhansari94@hotmail.com>
2021-01-26 12:53:49 +0300

    Load command deliver cache image on less seven days old cache

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -28,39 +28,30 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
   func test_load_failsOnRetrievalError() {
     let (sut, store) = makeSUT()
     let retrivalError = anyNSError()
-    let exp = expectation(description: "Wait for load completion")
-    var receivedError: Error?
-    sut.load { result in
-      switch result {
-      case let .failure(error):
-        receivedError = error
-      default:
-        XCTFail("Expected failure, got \(result) instead.")
-      }
-      exp.fulfill()
-    }
-    store.completeRetrival(with: retrivalError)
-    wait(for: [exp], timeout: 1.0)
-    XCTAssertEqual(receivedError as NSError?, retrivalError)
+    
+    expect(sut, toCompleteWith: .failure(retrivalError), when: {
+      store.completeRetrival(with: retrivalError)
+    })
   }
   
   func test_load_deliversNoImagesOnEmptyCache() {
     let (sut, store) = makeSUT()
-    let exp = expectation(description: "Wait for load completion")
-
-    var receivedImages: [FeedImage]?
-    sut.load { result in
-      switch result {
-      case let .success(images):
-        receivedImages = images
-      default:
-        XCTFail("Expected success, got \(result) instead.")
-      }
-      exp.fulfill()
-    }
-    store.completeRetrivalWithEmptyCache()
-    wait(for: [exp], timeout: 1.0)
-    XCTAssertEqual(receivedImages, [])
+    
+    expect(sut, toCompleteWith: .success([]), when: {
+      store.completeRetrivalWithEmptyCache()
+    })
+  }
+  
+  func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
+    let feed = uniqueImageFeed()
+    let fixedCurrentDate = Date()
+    let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+    let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+    
+    expect(sut, toCompleteWith: .success(feed.models), when: {
+      store.completeRetrival(with: feed.local, timestamp: fixedCurrentDate)
+    })
+    
   }
   
   // MARK: - Helpers 
@@ -72,7 +63,51 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     return (sut, store)
   }
   
+  private func expect(_ sut: LocalFeedLoader, toCompleteWith expectedResult: LocalFeedLoader.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    let exp = expectation(description: "Wait for load completion")
+    
+    sut.load { receivedResult in
+      switch (receivedResult, expectedResult) {
+      case let (.success(receivedImages), .success(expectedImages)):
+        XCTAssertEqual(receivedImages, expectedImages,  file: file, line: line)
+      case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+        XCTAssertEqual(receivedError, expectedError,  file: file, line: line)
+      default:
+        XCTFail("Expected result \(expectedResult) , got \(receivedResult) instead.",  file: file, line: line)
+      }
+      exp.fulfill()
+    }
+    
+    action()
+    wait(for: [exp], timeout: 1.0)
+  }
+  
   private func anyNSError() -> NSError {
     return NSError(domain: "any error", code: 0)
+  }
+  
+  private func uniqueImage() -> FeedImage {
+    return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+  }
+
+  private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let feed = [uniqueImage(), uniqueImage()]
+    let localItems = feed.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    return (feed, localItems)
+  }
+
+  private func anyURL() -> URL {
+    let url = URL(string: "www.any-url.com")!
+    return url
+  }
+  
+}
+private extension Date {
+  func adding(days: Int) -> Date {
+    return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+  }
+  
+  func adding(seconds: TimeInterval) -> Date {
+    return self + seconds
   }
 }
