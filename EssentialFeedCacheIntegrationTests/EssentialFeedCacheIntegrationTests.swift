b30219f6c5c0da5395e037d034b7672c4004ecb7
Ilhansari <ilhansari94@hotmail.com>
2021-02-14 12:34:15 +0300

    Replace custom `LoadFeedResult` enum with standard `Swift.Result`

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Ilhan Sari on 13.02.2021.
+//  Copyright © 2021 ilhan sarı. All rights reserved.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+  // FIX: - If necessary checkout here. (Persistence Module)
+    
+}
