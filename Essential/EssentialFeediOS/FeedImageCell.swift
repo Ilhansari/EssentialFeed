d5a9e0e07bd573395080ebe7a59d0e32a5a7affd
Ilhansari <ilhansari94@hotmail.com>
2021-02-24 09:58:43 +0300

    Render loaded feed on successful load completion

---
+++ Essential/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Ilhan Sari on 24.02.2021.
+//  Copyright © 2021 ilhan sarı. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
